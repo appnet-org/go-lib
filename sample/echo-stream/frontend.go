a4b8e95fe2f5c03da2dc23a71e3dfe451b68d246
Xiangfeng Zhu <xzhu0027@gmail.com>
2024-07-16 23:16:12 -0400

    Update echo stream app

--- sample/echo-stream/frontend.go
+++ sample/echo-stream/frontend.go
@@ -6,72 +6,102 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	echo "github.com/appnet-org/golib/sample/echo-stream/pb"
 	"google.golang.org/grpc"
 )
 
-var (
-	grpcClient echo.EchoServiceClient
-	stream     echo.EchoService_EchoClient
-)
+func handleHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
 
-func initGRPC() {
-	conn, err := grpc.Dial("server-stream.default.svc.cluster.local:9000", grpc.WithInsecure())
+	conn, err := grpc.DialContext(ctx, "server:9000", grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		http.Error(w, "Failed to connect to gRPC server", http.StatusInternalServerError)
+		log.Printf("Failed to connect to gRPC server: %v", err)
+		return
 	}
+	defer conn.Close()
 
-	grpcClient = echo.NewEchoServiceClient(conn)
-	stream, err = grpcClient.Echo(context.Background())
+	grpcClient := echo.NewEchoServiceClient(conn)
+	stream, err := grpcClient.Echo(context.Background())
 	if err != nil {
-		log.Fatalf("could not establish stream: %v", err)
+		http.Error(w, "Failed to establish stream", http.StatusInternalServerError)
+		log.Printf("Failed to establish stream: %v", err)
+		return
 	}
+	defer stream.CloseSend()
 
 	go func() {
 		for {
-			in, err := stream.Recv()
-			if err == io.EOF {
-				break
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				in, err := stream.Recv()
+				if err == io.EOF {
+					return
+				}
+				if err != nil {
+					log.Printf("Failed to receive a message: %v", err)
+					return
+				}
+				log.Printf("Got message: %s", in.Body)
 			}
-			if err != nil {
-				log.Fatalf("Failed to receive a message: %v", err)
-			}
-			log.Printf("Got message: %s", in.Body)
 		}
 	}()
-}
 
-func handleHTTP(w http.ResponseWriter, r *http.Request) {
 	requestBody := r.URL.Query().Get("key")
+	fmt.Printf("Frontend got request with key: %s\n", requestBody)
+
 	if requestBody == "" {
 		http.Error(w, "key parameter is required", http.StatusBadRequest)
 		return
 	}
 
-	for i := 0; i < 2; i++ {
+	for i := 0; i < 1; i++ {
 		msg := fmt.Sprintf("%s %d", requestBody, i+1)
 		if err := stream.Send(&echo.Msg{Body: msg}); err != nil {
-			log.Fatalf("Failed to send a message: %v", err)
+			log.Printf("Failed to send a message: %v", err)
+			http.Error(w, "Failed to send message", http.StatusInternalServerError)
+			return
 		}
 		log.Printf("Sent message: %s", msg)
 		time.Sleep(1 * time.Second) // Simulate some delay
 	}
 
-	fmt.Fprintf(w, "Sent message: %s", requestBody)
+	fmt.Fprintf(w, "Sent message: %s\n", requestBody)
 }
 
 func main() {
-	initGRPC()
-
 	http.HandleFunc("/", handleHTTP)
-	log.Println("HTTP server listening on port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("Failed to serve HTTP: %v", err)
+	server := &http.Server{
+		Addr: ":8080",
 	}
 
-	// Clean up the stream after the HTTP server is done
-	stream.CloseSend()
-	time.Sleep(1 * time.Second) // Allow some time to finish receiving messages
+	go func() {
+		log.Println("HTTP server listening on port 8080!!!")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to serve HTTP: %v", err)
+		}
+	}()
+
+	// Handle graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+
+	log.Println("Shutting down gracefully...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("HTTP server Shutdown: %v", err)
+	}
 }
--- sample/echo-stream/server.go
+++ sample/echo-stream/server.go
@@ -18,18 +18,22 @@ func (s *server) Echo(stream echo.EchoService_EchoServer) error {
 	for {
 		msg, err := stream.Recv()
 		if err == io.EOF {
+			log.Println("Client closed the connection (EOF)")
 			return nil
 		}
 		if err != nil {
+			log.Printf("Error receiving message: %v", err)
 			return err
 		}
 		log.Printf("Received message: %s", msg.Body)
 
 		// Send two responses for each request
-		for i := 0; i < 2; i++ {
+		for i := 0; i < 1; i++ {
 			responseMsg := &echo.Msg{Body: "Echo " + string(i+1) + ": " + msg.Body}
+			log.Printf("Sending message: %s", responseMsg.Body)
 			err = stream.Send(responseMsg)
 			if err != nil {
+				log.Printf("Error sending message: %v", err)
 				return err
 			}
 			time.Sleep(100 * time.Millisecond) // Simulate some delay
@@ -44,7 +48,7 @@ func main() {
 	}
 	s := grpc.NewServer()
 	echo.RegisterEchoServiceServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
+	log.Printf("Server listening at %v !!!", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
