70d455f135580f965956307d4833c5128618ebc0
jokerwyt <914554688@qq.com>
2024-09-03 15:41:37 -0600

    lb_random

---
+++ sample/lb_random/go.mod
@@ -0,0 +1,11 @@
+module appnet.wiki/lb_random
+
+go 1.22.1
+
+require google.golang.org/grpc v1.63.2
+
+require (
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
---
+++ sample/lb_random/lb_random.go
@@ -0,0 +1,78 @@
+/*
+ *
+ * Copyright 2017 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package main
+
+import (
+	"math/rand"
+	"sync"
+
+	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/balancer/base"
+	"google.golang.org/grpc/grpclog"
+)
+
+// Name is the name of appnet_lb balancer.
+const Name = "appnet_lb"
+
+var logger = grpclog.Component("appnetlb")
+
+// newBuilder creates a new appnet balancer builder.
+func NewBuilder(sharedData *sync.Map) balancer.Builder {
+	return base.NewBalancerBuilder(Name, &appnetlbPickerBuilder{sharedData: sharedData}, base.Config{HealthCheck: true})
+}
+
+type appnetlbPickerBuilder struct {
+	sharedData *sync.Map
+}
+
+func (b *appnetlbPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
+	logger.Warningf("testlbPicker: Build called with info: %v", info)
+	if len(info.ReadySCs) == 0 {
+		return base.NewErrPicker(balancer.ErrNoSubConnAvailable)
+	}
+	scs := make([]balancer.SubConn, 0, len(info.ReadySCs))
+	for sc := range info.ReadySCs {
+		scs = append(scs, sc)
+	}
+
+	return &appnetlbPicker{
+		subConns: scs,
+		// Start at a random index, as the same appnet balancer rebuilds a new
+		// picker when SubConn states change, and we don't want to apply excess
+		// load to the first server in the list.
+		next:       uint32(rand.Intn(len(scs))),
+		sharedData: b.sharedData,
+	}
+}
+
+type appnetlbPicker struct {
+	// subConns is the snapshot of the roundrobin balancer when this picker was
+	// created. The slice is immutable. Each Get() will do a round robin
+	// selection from it and return the selected SubConn.
+	subConns   []balancer.SubConn
+	next       uint32
+	sharedData *sync.Map
+}
+
+func (p *appnetlbPicker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
+	subConnsLen := uint32(len(p.subConns))
+	random := rand.Intn(int(subConnsLen))
+	sc := p.subConns[random]
+	return balancer.PickResult{SubConn: sc}, nil
+}
